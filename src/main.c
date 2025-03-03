8babf1cad9aa09d403d0c734ae2fe55fc4711f8e
babidisrc <thaissa.linguika@gmail.com>
2025-03-03 13:39:44 -0300

    sorting algorithm

--- src/main.c
+++ src/main.c
@@ -9,7 +9,47 @@
 const int low_threshold = 127;
 const int high_threshold = 223;
 
-int main (int argc, char *argv[]) {
+unsigned char getBrightness(unsigned char *pixel, int channels) {
+    if (channels == 1) return pixel[0]; // Grayscale
+    if (channels == 3 || channels == 4) {
+        return (pixel[0] + pixel[1] + pixel[2]) / 3; // RGB(A)
+    }
+    return 0;
+}
+
+int comparePixels(const void *a, const void *b) {
+    unsigned char *pixelA = (unsigned char *)a;
+    unsigned char *pixelB = (unsigned char *)b;
+    
+    unsigned char brightnessA = get_brightness(pixelA, 3);
+    unsigned char brightnessB = get_brightness(pixelB, 3);
+    
+    return brightnessA - brightnessB;
+}
+
+void sortInterval(unsigned char *img, int start_idx, int end_idx, int channels) {
+    int num_pixels = (end_idx - start_idx) + 1;
+
+    unsigned char *pixels = malloc(num_pixels * channels);
+
+    for (int i = 0; i < num_pixels; i++) {
+        for (int c = 0; c < channels; c++) {
+            pixels[i * channels + c] = img[(start_idx + i) * channels + c];
+        }
+    }
+
+    qsort(pixels, num_pixels, channels * sizeof(unsigned char), comparePixels);
+
+    for (int i = 0; i < num_pixels; i++) {
+        for (int c = 0; c < channels; c++) {
+            img[(start_idx + i) * channels + c] = pixels[i * channels + c];
+        }
+    }
+    
+    free(pixels);
+}
+
+int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Usage: ./pixel-sorting <image.jpg>");
         exit(1);
@@ -54,6 +94,25 @@ int main (int argc, char *argv[]) {
                 mask_p[2] = 0;
             }
         }
+    }
+
+    int i = 0;
+    int j = 0;
+    while (i < width * height) {
+        unsigned char *p = img + (channels * (j * width + i));
+        unsigned char *mask_p = mask + (channels * (j * width + i));
+
+        unsigned char arr[width * height];
+        int aux = 0;
+
+        if (&mask_p == 255) {
+            arr[i] = aux++;
+            if (arr[i + 1] == 0) {
+                aux = 0;
+                i++;
+            }
+        }
+        j++;
     }
 
     stbi_write_jpg("../data/sorted-image.jpg", width, height, channels, mask, 100);
